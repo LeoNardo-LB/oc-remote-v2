c70a73b595c2ec5e787596556e5307ece1c0543b
leonardo <tab206388@aliyun.com>
2026-05-28 03:40:42 +0800

    v2.0.0-beta.40: fix streaming wasAtBottom tracking, fix broken table, terminal in menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.minios.ocremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 239
-        versionName = "2.0.0-beta.39"
+        versionCode = 240
+        versionName = "2.0.0-beta.40"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
