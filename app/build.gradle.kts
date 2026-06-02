f2b010bd45784af038687997736a5dd1075cddad
leonardo <tab206388@aliyun.com>
2026-06-02 11:39:47 +0800

    release: v2.0.0-beta.120

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.minios.ocremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 319
-        versionName = "2.0.0-beta.119"
+        versionCode = 320
+        versionName = "2.0.0-beta.120"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
