c8297882a8a2c82d76396f3eb3f600b669458620
leonardo <tab206388@aliyun.com>
2026-06-09 00:17:04 +0800

    chore: bump version to v2.0.0-beta.167

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.minios.ocremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 366
-        versionName = "2.0.0-beta.166"
+        versionCode = 367
+        versionName = "2.0.0-beta.167"
 
         testInstrumentationRunner = "dev.minios.ocremote.HiltTestRunner"
         vectorDrawables {
